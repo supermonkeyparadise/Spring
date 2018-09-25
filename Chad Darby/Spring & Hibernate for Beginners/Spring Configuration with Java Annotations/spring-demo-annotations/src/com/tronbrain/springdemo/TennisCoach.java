3c673e72befaa9f74b4610ace8294e0c0ff1ca0f
Unknown <steven.chou@uniqestate.com>
2018-09-25 21:21:13 +0800

    Bean Lifecycle Method Annotations

--- Chad Darby/Spring & Hibernate for Beginners/Spring Configuration with Java Annotations/spring-demo-annotations/src/com/tronbrain/springdemo/TennisCoach.java
+++ Chad Darby/Spring & Hibernate for Beginners/Spring Configuration with Java Annotations/spring-demo-annotations/src/com/tronbrain/springdemo/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.tronbrain.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
@@ -16,6 +19,18 @@ public class TennisCoach implements Coach {
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
 	}
 
 	/*
